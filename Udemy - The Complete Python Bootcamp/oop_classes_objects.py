d6b5773726de01701268ce1500eb46a0e5c16ce1
Karl Steven F. Requinto <lerdrobott@gmail.com>
2023-06-05 04:34:25 +0800

    reviewing oop

--- Udemy - The Complete Python Bootcamp/oop_classes_objects.py
+++ Udemy - The Complete Python Bootcamp/oop_classes_objects.py
@@ -9,12 +9,15 @@
 # OOP DEFINING CLASSES AND OBJECTS
 class Robot:
     """THIS CLASS IMPLEMENTS A ROBOT"""
+    population = 0
 
-    def __init__(self, name, year):
+    def __init__(self, name, year):  # CONSTRUCTOR
         self.name = name
         self.year = year
+        Robot.population += 1
 
 
 r1 = Robot("R1", 2023)
 
 print(r1.__dict__)
+print(Robot.population)
